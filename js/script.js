64124c4fb787ea901662c991fd068314754da2c3
Marko Mrkonjić <markomrkonjic9@gmail.com>
2017-10-05 19:07:22 +0200

    api changed

--- js/script.js
+++ js/script.js
@@ -22,7 +22,8 @@
   ZEALOT.idOperatorForTicket = 0;
   ZEALOT.idPriorityForTicket = 0;
 
-  ZEALOT.apiRoot = "http://localhost:50358/api/";
+  ZEALOT.apiRoot = "https://zealott.azurewebsites.net/api/";
+  //ZEALOT.apiRoot = "http://localhost:50358/api/";
   ZEALOT.userInfo = "";
   ZEALOT.allOperators = "";
   ZEALOT.allSectors = "";
